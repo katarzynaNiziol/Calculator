bacde6404137899eccb4b1701cfdeaf5a3bfc127
Katarzyna <katarzyna.niziol1993@gmail.com>
2022-06-09 07:58:55 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our \"Zombie\" program!!! ");
+
+        String backpack[] = {"Shotgun", "Assault Riffle", "Sniper", "ACS", "SPAS-12"};
+        String zombies[] = {"Close-range zombie", "Mid-range zombie", "Long-range zombie"};
+        int numbersZombiesHate[] = {4, 34, 56, 78, 3, 567, 1};
+
+
+        System.out.println("");
+        System.out.println("************");
+        System.out.println("Present Backpack items! ");
+        System.out.println(backpack[0]);
+        System.out.println(backpack[1]);
+        System.out.println(backpack[2]);
+        System.out.println(backpack[3]);
+        System.out.println(backpack[4]);
+        System.out.println("");
+        System.out.println("************");
+        System.out.println("Zombies outside home...");
+        System.out.println(zombies[0]);
+        System.out.println(zombies[1]);
+        System.out.println(zombies[2]);
+        System.out.println("");
+        System.out.println("************");
+        System.out.println(numbersZombiesHate[6]);
+    }
+}
